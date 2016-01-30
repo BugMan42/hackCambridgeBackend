bfaf0036a6be52348e79afbfd597d28a40380c86
Ricard Gascons <gascons1995@gmail.com>
2016-01-30 16:49:20 +0000

    Update rest server

--- relatedWords/restAlgoServer.py
+++ relatedWords/restAlgoServer.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from createCoOccurrence import WordRecommender
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
+	word = request.form['word']
+	nRecom = request.form['nrecom']
 	obj = WordRecommender()
 	obj.run()
 

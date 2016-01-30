9f8159c2f750e28215f53dd165d4c633c3b43244
Ricard Gascons <gascons1995@gmail.com>
2016-01-30 20:37:16 +0000

    Add post method to related endpoint

--- backend/routes/related.js
+++ backend/routes/related.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var word = require('mongoose').model('WordEN');
+
+String.prototype.format = function () {
+    var i = 0, args = arguments;
+    return this.replace(/{}/g, function () {
+        return typeof args[i] != 'undefined' ? args[i++] : '';
+    });
+};
 
 // get all related words
-router.get('/', function(req, res, next){
-    /*word.find({}, function(err, data) {
+//var word = req.body.word;
+router.post('/', function(req, res, next){
+    word.find({}, function(err, data) {
         if(!err) {
-            res.status(200).json(data);
+            var randWord = data[Math.floor(Math.random() * data.length)];
+            var xmlHttp = new XMLHttpRequest();
+            var NUMBER_OF_WORDS = 50;
+            xmlHttp.open("POST", "http://172.20.1.43:5000", false);
+            xmlHttp.send("fname={}&lname={}".format(randWord, NUMBER_OF_WORDS));
+            var response =  xmlHttp.responseText;
+            res.status(200).end(response);
         }
-    })*/
+        else {
+            res.status(404).end("Not Found");
+        }
+    });
+
+
 });
 
 

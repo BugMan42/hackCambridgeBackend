a2a7bc52d017f55b10f9fec1ce5470ea4db7429f
Ricard Gascons <gascons1995@gmail.com>
2016-01-30 14:53:32 +0000

    Add models and setup app.js

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var fs = require('fs');
+var http = require('http');
+var bodyParser = require('body-parser');
+
+var app = express();
+require('./models/index').initialize();
+
+mongoose.connect('mongodb://localhost/backend');
+
+app.use(bodyParser.json());
+
+http.createServer(app).listen(8080);
--- backend/models/user.wordsEN.js
+++ backend/models/user.wordsEN.js
@@ -2,5 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = function() {
+    var wordsENSchema = new Schema({
+        word: {type: String, required: true},
+        definition: {type: String, required: false},
+        audio: {data: Buffer, contentType: String}
+    });
 
+    mongoose.model('WordEN', wordsENSchema, 'words');
 };

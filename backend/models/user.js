efea4630008b77856465f60597460c8cdfe7bffa
BugMan42 <guillem812@gmail.com>
2016-01-30 14:43:39 +0000

    models + route

---
+++ backend/models/user.js
@@ -0,0 +1,16 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+/**
+ * Function to define the model of an User.
+ */
+module.exports = function() {
+    var userSchema = new Schema({
+        username: {type: String, required:true},
+        password: {type: String, required: true},
+        is_admin: {type: Boolean}
+    });
+
+    mongoose.model('User', userSchema, 'users');
+};
